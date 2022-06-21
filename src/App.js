acf9f7dd67896f19a65d588b3b1c2e219ab6ffff
Karpis Karapetyan <karpis.karpis93@gmail.com>
2022-06-21 20:56:03 +0400

    currentCom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { useRoutes } from 'react-router-dom';
-import { Header } from './Components/Header/Header';
+import  Header  from './Components/Header/Header';
 import { ROUTES } from './helpers/routes';
 
 function App() {
   const routes = useRoutes(ROUTES)
   return (
     <div className="App">
-      <Header/>
-      {routes}
+        <Header />
+        {routes}
     </div>
   );
 }
